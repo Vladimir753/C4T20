c03ee9d717410a1251e247f4dde289dcc5e20293
Vladimir Marinov <vladimir753@abv.bg>
2017-10-07 14:34:43 +0300

    database

---
+++ database/migrations/2017_10_07_105414_create_table_company.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableCompany extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('company', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('phone');
+            $table->string('email');
+            $table->text('address');
+            $table->string('city');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('company', function (Blueprint $table) {
+            Schema::dropIfExists('company');
+        });
+    }
+}
